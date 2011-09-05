916e2da542170999b8552447f4cf1f8761d0aee2
Ioannis Canellos <iocanel@apache.org>
2011-09-05 20:34:31 +0000

    [SMX4-909] Changed OpenJpa bundle activator to export the javax.persistence.spi.PersistenceProvider service.
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/bundles/trunk/openjpa-1.2.1@1165412 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/bundles/openjpa/Activator.java
+++ src/main/java/org/apache/servicemix/bundles/openjpa/Activator.java
@@ -18,19 +18,20 @@
  */
 package org.apache.servicemix.bundles.openjpa;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.persistence.spi.PersistenceProvider;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.openjpa.persistence.PersistenceProductDerivation;
+import org.apache.openjpa.persistence.PersistenceProviderImpl;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 
 /**
@@ -38,12 +39,23 @@ import org.osgi.framework.BundleContext;
  * persistent configuration file
 */
 public class Activator extends org.apache.servicemix.specs.locator.Activator {
+    // following is so Aries can find and extend us for OSGi RFC 143
+    public static final String PERSISTENCE_PROVIDER_ARIES = "javax.persistence.provider";
+    // following would be set by Aries to expose their OSGi enabled provider
+    public static final String PERSISTENCE_PROVIDER = PersistenceProvider.class.getName();
+    public static final String OSGI_PERSISTENCE_PROVIDER = PersistenceProviderImpl.class.getName();
+    private static ServiceRegistration svcReg = null;
+
     private static final transient Log LOG = LogFactory.getLog(Activator.class);
     private Map<Long, URL> persistenceFiles = new ConcurrentHashMap<Long, URL>();
-   
+    
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         super.start(bundleContext);
+        PersistenceProvider provider = new PersistenceProviderImpl();
+        Hashtable<String, String> props = new Hashtable<String, String>();
+        props.put(PERSISTENCE_PROVIDER_ARIES, OSGI_PERSISTENCE_PROVIDER);
+        svcReg = bundleContext.registerService(PERSISTENCE_PROVIDER, provider, props);
         LOG.info("OpenJPA activator starting");
     }
 
@@ -51,6 +63,7 @@ public class Activator extends org.apache.servicemix.specs.locator.Activator {
     public void stop(BundleContext bundleContext) throws Exception {
         super.stop(bundleContext);
         PersistenceProductDerivation.setDefaultPersistenceFiles(null);
+        svcReg.unregister();
         LOG.info("OpenJPA activator stopping");
     }
 
